2d10eee648c719ba8054cd2e03706c2b24bafc77
Mohammed Seoud <m.seoud42@gmail.com>
2024-03-02 00:59:26 +0200

    FirstCommit

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,9 @@ function App() {
 
   return (
     <>
-      <div className=' bg-black bg-blend-saturation p-5 text-white  w-full h-fit text-7xl'>
-
-<Header/>
-<TableList/>
+      <div className=' bg-black min-w-[800px] bg-blend-saturation p-5 text-white  w-full h-fit text-7xl'>
+      <Header/>
+      <TableList/>
 
 
        </div>
